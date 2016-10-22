fb54ce2a5910bb96b8a2659f675584d3c7562fe5
Michael Roberts <michael@vivtek.com>
2016-10-22 11:46:37 -0400

    Converted klatsch to POE

--- klatsch.pl
+++ klatsch.pl
@@ -1,25 +1,89 @@
 #!/usr/bin/perl
 
-$|=1;
-$peer = $ENV{'REMOTE_HOST'};
-respond (100, "Welcome to Coffeebot. Service is my only joy.");
+use strict;
+use warnings;
+my $peer = $ENV{'REMOTE_HOST'};
 
-sub respond {
+use POE;
+use POE::Wheel::ReadWrite;
+
+# Open free port for binary data channel
+
+# Create session for console (talking to the client)
+POE::Session->create (
+    inline_states => {
+        _start        => \&console_start,
+        console_input => \&console_handler,
+        _stop         => \&console_stop,
+    }
+);
+
+# Create session on serial port for the visual cortex
+
+# Create session on serial port for the motor ganglion
+
+
+$poe_kernel->run();
+exit 0;
+
+sub shut_down {
+    $poe_kernel->stop();
+}
+
+
+
+# ---------------------------------------------------------------------------------------
+# Console handling
+# ---------------------------------------------------------------------------------------
+my $console = undef;
+sub say {
+    return unless $console;
     my ($num, $data, $text) = @_;
     if (defined $text) {
-        print "$num [$data] $text\r\n";
+        $console->put ("$num [$data] $text");
     } else {
-        print "$num $data\r\n";
+        $console->put ("$num $data");
+    }
+    $console->flush();
+}
+
+sub console_start {
+    my ($heap) = $_[HEAP];
+    $heap->{console} = POE::Wheel::ReadWrite->new(
+        InputHandle  => \*STDIN,
+        OutputHandle => \*STDOUT,
+        InputEvent   => 'console_input',
+        ErrorEvent   => 'console_error',
+    );
+    $console = $heap->{console};
+    say (100, "Welcome to Coffeebot - service is my only joy");
+}
+
+sub console_stop {
+    delete $_[HEAP]->{console};
+}
+
+sub console_handler {
+    my ($heap, $kernel, $input, $exception) = @_[HEAP, KERNEL, ARG0, ARG1];
+    if (defined $input) {
+        my @pieces = split / /, $input;
+        my $command = shift @pieces;
+        if ($command eq 'quit' || $command eq 'bye' || $command eq 'exit') {
+            command_quit ();
+        } elsif ($command eq 'something') {
+            # command_something($heap, @pieces);
+        } else {
+            say (500, "Unknown command '$command'");
+        }
     }
 }
 
-while (<>) {
-    s/[\n\r]+$//;
-    my @pieces = split;
-    my $command = shift @pieces;
-    last if $command eq 'quit';
-    last if $command eq 'bye';
-    last if $command eq 'exit';
+sub console_error {
+    my ($kernel, $heap) = @_[KERNEL, HEAP];
+    $kernel->yield('_stop');
+}
 
-    respond (500, "Unknown command $command");
+sub command_quit {
+    say (999, "Only joy");
+    shut_down();
 }
