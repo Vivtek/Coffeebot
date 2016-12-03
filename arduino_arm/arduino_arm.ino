fedd0df97dbe18c1eb747ccfc8e05dd2a4e92c57
Michael Roberts <michael@vivtek.com>
2016-12-03 14:18:40 -0400

    Better motor speed coordination, values using '=N val' format

--- arduino_arm/arduino_arm.ino
+++ arduino_arm/arduino_arm.ino
@@ -2,10 +2,10 @@
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x62); 
-Adafruit_DCMotor *M1 = AFMS.getMotor(4);
-Adafruit_DCMotor *M2 = AFMS.getMotor(3);
-Adafruit_DCMotor *M3 = AFMS.getMotor(2);
-Adafruit_DCMotor *M4 = AFMS.getMotor(1);
+Adafruit_DCMotor *M[4] = { AFMS.getMotor(4),
+                           AFMS.getMotor(3),
+                           AFMS.getMotor(2),
+                           AFMS.getMotor(1) };
 
 
 void setup() {
@@ -20,10 +20,8 @@ void setup() {
 // ----- state ------
 int speed = 100;
 
-int M1_on = 0;
-int M2_on = 0;
-int M3_on = 0;
-int M4_on = 0;
+int M_on[4] = { 0, 0, 0, 0 };
+char label[4][4] = { "MA1", "MA2", "MA3", "MA4" };
 
 unsigned long counter = 0;
 
@@ -31,87 +29,36 @@ void loop() {
   int command;
   if (millis() - counter > 10000) {
      counter = millis();
-     Serial.print   ("=A0 ");
-     Serial.println (analogRead(0));
-     Serial.print   ("=A1 ");
-     Serial.println (analogRead(1));
-     Serial.print   ("=A2 ");
-     Serial.println (analogRead(2));
+     announce ((char*)"A0", analogRead(0));
+     announce ((char*)"A1", analogRead(1));
+     announce ((char*)"A2", analogRead(2));
   }
   if (Serial.available()) {
      command = Serial.read();
      switch (command) {
        case '1':
-         M1_on = M1_on ? 0 : 1;
-         if (M1_on) {
-            M1->setSpeed(speed);
-            M1->run(FORWARD);
-         } else {
-            M1->run(RELEASE);
-         }
+         set_motor_speed (0, M_on[0] ? 0.0 : 1.0);
          break;
        case '2':
-         M2_on = M2_on ? 0 : 1;
-         if (M2_on) {
-            M2->setSpeed(speed);
-            M2->run(FORWARD);
-         } else {
-            M2->run(RELEASE);
-         }
+         set_motor_speed (1, M_on[1] ? 0.0 : 1.0);
          break;
        case '3':
-         M3_on = M3_on ? 0 : 1;
-         if (M3_on) {
-            M3->setSpeed(speed);
-            M3->run(FORWARD);
-         } else {
-            M3->run(RELEASE);
-         }
+         set_motor_speed (2, M_on[2] ? 0.0 : 1.0);
          break;
        case '4':
-         M4_on = M4_on ? 0 : 1;
-         if (M4_on) {
-            M4->setSpeed(speed);
-            M4->run(FORWARD);
-         } else {
-            M4->run(RELEASE);
-         }
+         set_motor_speed (3, M_on[3] ? 0.0 : 1.0);
          break;
        case 'q':
-         M1_on = M1_on ? 0 : 1;
-         if (M1_on) {
-            M1->setSpeed(speed);
-            M1->run(BACKWARD);
-         } else {
-            M1->run(RELEASE);
-         }
+         set_motor_speed (0, M_on[0] ? 0.0 : -1.0);
          break;
        case 'w':
-         M2_on = M2_on ? 0 : 1;
-         if (M2_on) {
-            M2->setSpeed(speed);
-            M2->run(BACKWARD);
-         } else {
-            M2->run(RELEASE);
-         }
+         set_motor_speed (1, M_on[1] ? 0.0 : -1.0);
          break;
        case 'e':
-         M3_on = M3_on ? 0 : 1;
-         if (M3_on) {
-            M3->setSpeed(speed);
-            M3->run(BACKWARD);
-         } else {
-            M3->run(RELEASE);
-         }
+         set_motor_speed (2, M_on[2] ? 0.0 : -1.0);
          break;
        case 'r':
-         M4_on = M4_on ? 0 : 1;
-         if (M4_on) {
-            M4->setSpeed(speed);
-            M4->run(BACKWARD);
-         } else {
-            M4->run(RELEASE);
-         }
+         set_motor_speed (3, M_on[3] ? 0.0 : -1.0);
          break;
        case 'x':
          do_stop();
@@ -122,14 +69,33 @@ void loop() {
 }
 
 void do_stop() {
-  M1_on = 0;
-  M1->run(RELEASE);
-  M2_on = 0;
-  M2->run(RELEASE);
-  M3_on = 0;
-  M3->run(RELEASE);
-  M4_on = 0;
-  M4->run(RELEASE);
+  set_motor_speed (0, 0.0);
+  set_motor_speed (1, 0.0);
+  set_motor_speed (2, 0.0);
+  set_motor_speed (3, 0.0);
+}
+
+void set_motor_speed (int motor, float mspeed) {
+  int new_speed = (int) (mspeed * (float) speed);
+  if (abs(new_speed) < 5) { new_speed = 0; }
+  if (new_speed == 0) { M[motor]->run(RELEASE); } // If the motor should be shut off, don't get confused.
+  if (new_speed == M_on[motor]) { return; }
+  announce (label[motor], new_speed);
+  M_on[motor] = new_speed;
+  if (new_speed < 0) {
+     M[motor]->setSpeed (-new_speed);
+     M[motor]->run (BACKWARD);
+  } else {
+     M[motor]->setSpeed (new_speed);
+     M[motor]->run (FORWARD);
+  }
+}
+
+void announce (char* name, int value) {
+  Serial.print ("=");
+  Serial.print (name);
+  Serial.print (" ");
+  Serial.println (value);
 }
 
 
